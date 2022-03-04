173e6e250e7dfa26f5a3302aaa0b03a6dd9e38af
Saauren Mankad <sman0039@student.monash.edu>
2022-03-04 11:18:02 +1100

    Completed Extension Part 2 (Calculating Age)

--- FIT3178-W01-Lab/Person.swift
+++ FIT3178-W01-Lab/Person.swift
@@ -16,18 +16,41 @@ class Person: NSObject {
         birthDate = newDate
     }
     
+    func calculateAge(myDate: Date) -> Int {
+        // calculates the age based on the birthdate provided.
+        // returns age in years
+        
+        let myCalendar = Calendar.current
+        let ageComponents = myCalendar.dateComponents([.year], from: myDate, to: Date())
+                
+        // return age
+        return ageComponents.year!
+        
+    }
     
+    
+    func calculateDaysAlive(myDate: Date) -> Int {
+        
+        
+        let myCalendar = Calendar.current
+        let dayComponents = myCalendar.dateComponents([.day], from: myDate, to: Date())
+        
+        return dayComponents.day!
+        
+    }
     
     func greeting() -> String {
-
+        
+        // prepares and returns greeting for the Person initialised
+        
         let formatter = DateFormatter()
         formatter.locale = Locale.current
         formatter.dateFormat = "dd/MM/yyyy"
         let prettyDate = formatter.string(from: birthDate)
+        let age = calculateAge(myDate: birthDate)
+        let daysAlive = calculateDaysAlive(myDate: birthDate)
         
-        
-        
-        return "Hello \(name). You were born on \(prettyDate)."
+        return "Hello \(name). You were born on \(prettyDate). This means you are \(age) years old. You have been alive for \(daysAlive) days."
 
     }
     
